398f706f05a4214dea93907374ece91efe136e67
LAM LE TRAN <tranlam181@gmail.com>
2019-08-29 15:46:39 +0700

    change token of 3C

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -4,5 +4,5 @@ export default class AppConfig {
     // static baseUrl = 'https://c3.mobifone.vn/crm/api'
     // static baseUrlAuth = 'https://c3.mobifone.vn/crm/auth'
     static hotline3C = '0768511234'
-    static baseUrl3C = `https://3c-web1.mobifone.vn/${AppConfig.hotline3C}/c2call?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpcHBob25lIjoiNDIwMV8xIn0.SnemWC3ywSO1mDWu_EcLKlSzpAZnHi395m9Z7TeD_ZM&number=`
+    static baseUrl3C = `https://3c-web1.mobifone.vn/${AppConfig.hotline3C}/c2call?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpcHBob25lIjoiNDIwMV8yIn0.5TRI6vdt2od0aPaAkEodup6yjkoN_wmeIiGEkiCfNk0&number=`
 }
